46c69121e09e5545fd5e3c91b4585687bbcb3a02
Igor <igorpimentel@id.uff.br>
2021-03-02 17:20:05 -0300

    Removed forgot my password link

--- src/pages/SignIn/styles.ts
+++ src/pages/SignIn/styles.ts
@@ -16,16 +16,6 @@ export const Title = styled.Text`
   margin: 64px 0 24px;
 `;
 
-export const ForgotPassword = styled.TouchableOpacity`
-  margin-top: 24px;
-`;
-
-export const ForgotPasswordText = styled.Text`
-  color: #f4ede8;
-  font-family: 'RobotoSlab-Regular';
-  font-size: 16px;
-`;
-
 export const CreateAccountButton = styled.TouchableOpacity`
   position: absolute;
   left: 0;
